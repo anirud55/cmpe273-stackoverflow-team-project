3db4c3c6c5d229844e00f99de22b2b55fc75b0eb
sakshikasat <kasat.sakshi94@gmail.com>
2022-04-25 19:01:47 -0700

    Creating home page layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,32 +1,32 @@
 import React from "react";
 import './App.css';
-import {BrowserRouter as Router,Switch, Route, Redirect} from "react-router-dom"
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom"
 import Home from "./Components/Home";
 import Login from "./auth/Login";
-import {isAutheticated} from './auth/helper/authapicalls'
+import { isAutheticated } from './auth/helper/authapicalls'
 import Signup from "./auth/Signup";
 function App() {
   return (
-   <Router>
-     <Switch>
-      <Route>
-        <Route exact path="/">
-        {isAutheticated() ? <Home /> : <Redirect to={{
+    <Router>
+      <Switch>
+        <Route>
+          <Route exact path="/">
+            {isAutheticated() ? <Home /> : <Redirect to={{
               pathname: "/login"
             }}
-          />}
-        </Route> :  
-        <Route exact 
-        path="/login">
-          <Login />
+            />}
+          </Route>
+          <Route exact
+            path="/login">
+            <Login />
+          </Route>
+          <Route exact
+            path="/signup">
+            <Signup />
+          </Route>
         </Route>
-        <Route exact 
-        path="/signup">
-          <Signup />
-        </Route>
-      </Route>
-     </Switch>
-   </Router>
+      </Switch>
+    </Router>
   );
 }
 
--- frontend/src/Components/Navbar.js
+++ frontend/src/Components/Navbar.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import './Navbar.css'
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <div>
+      <nav className='navbar'>
+        {/* Logo */}
+        <Link className='navbar-link' to="/">
+          <img className="navbar-logo" src={'./logostackoverflow.png'} alt="stackoverflow" />
+        </Link>
+        {/* Searchbar */}
+        {/* UserSection */}
+      </nav>
+    </div>
   )
 }
 
