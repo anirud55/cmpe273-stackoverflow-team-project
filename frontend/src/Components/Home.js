abffd8cb69040401991d6f81edf1bc0adff7eee4
vineetkarmiani <vineetkarmiani@gmail.com>
2022-04-27 01:16:40 -0700

    Adding text and button

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -11,8 +11,18 @@ function Home() {
       </Row>
       <Row className='Home_Sidebar'>
         <Col className='Home_Sidebar_Col' md={2}>
-        <Sidebar></Sidebar>
-
+          <Sidebar></Sidebar>
+        </Col>
+        <Col className='Home_Questions_Col' md={8}>
+          <Row  className='Home_Questions_Col_Tabs' md={2}>
+            <Col md={6}>
+              <div className='Home_Questions_Col_Tabs_Text'>Top Questions</div>
+            </Col>
+            <Col md={4}></Col>
+            <Col md={2}>
+              <Button className='Home_Questions_Col_Tabs_Button'>Ask Question</Button>
+            </Col>
+          </Row>
         </Col>
       </Row>
     </Container>
