0a4ec6c17cad918df95ab1ffe4e3af8a2bfd4937
vineetkarmiani <vineetkarmiani@gmail.com>
2022-04-28 22:41:58 -0700

    Adding tags component

--- frontend/src/Components/Tags.js
+++ frontend/src/Components/Tags.js
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { API } from "../../src/backend";
 
 function Tags() {
+  const [tags, setTags] = useState([]);
+
+  const getAllTags = ()=>{
+    return fetch(`${API}/tags`, {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      }
+    })
+      .then(response => {
+        return response.json();
+      })
+      .catch(err => console.log(err));
+  }
+
+  useEffect(()=>{
+    getAllTags();
+  },[])
   return (
-    <div>Tags</div>
+    <div>
+      {tags && tags.map((tag)=>{
+        return <p>{tag?.tagname}</p>
+      })}
+    </div>
   )
 }
 
