87c3a040e37c7dc05eaa158207f834dadc8ef6f6
sohamkasar19 <sohamkasar19@gmail.com>
2022-05-03 00:08:33 -0700

    add navbar before routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,14 @@
 import React from "react";
-import './App.css';
-import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom"
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 import Home from "./Components/Home";
 import Login from "./auth/Login";
-import { isAutheticated } from './auth/helper/authapicalls'
+import { isAutheticated } from "./auth/helper/authapicalls";
 import Signup from "./auth/Signup";
 import NavBar from "./Components/Navbar";
 import Tags from "./Components/Tags";
@@ -12,48 +17,48 @@ import Users from "./Components/Users";
 import QuestionOverview from "./Components/QuestionOverview";
 function App() {
   return (
-    <Router>
-      <Switch>
+    <div>
+      <Router>
+        <NavBar />
+        <Switch>
           <Route exact path="/">
-            {true ? <Home /> : <Redirect to={{
-              pathname: "/login"
-            }}
-            />}
+            {true ? (
+              <Home />
+            ) : (
+              <Redirect
+                to={{
+                  pathname: "/login",
+                }}
+              />
+            )}
           </Route>
-          <Route exact
-            path="/login">
+          <Route exact path="/login">
             <Login />
           </Route>
-          <Route exact
-            path="/signup">
+          <Route exact path="/signup">
             <Signup />
           </Route>
-          <Route exact 
-        path="/navbar">
-          <NavBar />
-        </Route>
-        <Route exact 
-        path="/tags">
-          <Tags />
-        </Route>
-        <Route exact 
-        path="/users">
-          <Users />
-        </Route>
-        <Route exact 
-        path="/companies">
-          <Tags />
-        </Route>
-        <Route exact 
-        path="/question/ask">
-          <AskQuestion />
-        </Route>
-        <Route exact 
-        path="/questionOverview">
-          <QuestionOverview />
-        </Route>
-      </Switch>
-    </Router>
+          <Route exact path="/navbar">
+            <NavBar />
+          </Route>
+          <Route exact path="/tags">
+            <Tags />
+          </Route>
+          <Route exact path="/users">
+            <Users />
+          </Route>
+          <Route exact path="/companies">
+            <Tags />
+          </Route>
+          <Route exact path="/question/ask">
+            <AskQuestion />
+          </Route>
+          <Route exact path="/questionOverview">
+            <QuestionOverview />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
   );
 }
 
