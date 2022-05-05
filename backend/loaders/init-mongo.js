2ba94eb599a3b06e94b967999a405bde46da80df
sakshikasat <kasat.sakshi94@gmail.com>
2022-05-04 23:46:47 -0700

    Add connection pooling to mongo

--- backend/loaders/init-mongo.js
+++ backend/loaders/init-mongo.js
@@ -6,10 +6,7 @@ import config from '../config/db.mongo.json' assert { type: 'json' };
 const env = process.env.ENV || 'dev'
 const db = config[env]
 
-const mongo = mongoose.connect(db['uri'], {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => {
+const mongo = mongoose.connect(db['uri'], db['connectionOptions']).then(() => {
   console.log('Mongo connection ... OK');
 }).catch((err) => { console.log(err) })
 
