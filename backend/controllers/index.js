4d55f9e7ba93f09371b73102bf35245d0a0bbd32
anirud55 <90978843+anirud55@users.noreply.github.com>
2022-05-07 15:15:15 -0700

    added admin login

--- backend/controllers/index.js
+++ backend/controllers/index.js
@@ -2,10 +2,12 @@ import express from 'express';
 import authController from './authController'
 import tagController from './tagController';
 import postController from './postController';
+import adminController from './adminController';
 const router = new express.Router();
 
 router.use('/auth', authController)
 router.use('/tags', tagController);
 router.use('/posts', postController);
+router.use('/admin', adminController)
 
 export default router;
