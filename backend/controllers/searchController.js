2af227287533219aa4b538469717893436d433b3
Unmesh Padhye <unmeshpadhye@gmail.com>
2022-05-07 17:57:54 -0700

    search query post request through kafka

--- backend/controllers/searchController.js
+++ backend/controllers/searchController.js
@@ -5,7 +5,18 @@ router.get('/', async (req, res) => {
     res.status(200).json(result);
 });
 
+// router.post('/', async (req, res) => {
+//     const input = req.body;
+//     const result = await searchQuestions(input);
+// });
+
 router.post('/', async (req, res) => {
-    const input = req.body;
-    const result = await searchQuestions(input);
+    const { key, tag, user, isAccepted } = req.body;
+    sendRequest('search', { key, tag, user, isAccepted, action: 'SEARCH' }, (err, data) => {
+        if (err) {
+            res.status(400).json(err);
+        }
+        else
+            res.status(200).json(data);
+    });
 });
