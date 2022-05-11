dfc5f5e2a5704b2e5287edb75eb3cb699c30d027
anirud55 <90978843+anirud55@users.noreply.github.com>
2022-05-11 01:49:45 -0700

    approve question

--- kafka/consumers/adminConsumer.js
+++ kafka/consumers/adminConsumer.js
@@ -1,5 +1,5 @@
 import { getConsumer, getProducer } from '../loaders/kafka';
-import { getuaqs } from '../services/adminService';
+import { getuaqs, approve_question } from '../services/adminService';
 
 getConsumer('admin', (consumer) => {
   var producer = getProducer()
@@ -12,6 +12,35 @@ getConsumer('admin', (consumer) => {
 
     if (action === 'GET_UA_POSTS') {
         getuaqs((err, res) => {
+        var payload = {};
+        if (err) {
+          console.log('Get all Unapproved posts failed:', err)
+          payload = {
+            status: 400,
+            content: err,
+            correlationId: correlationId
+          }
+        }
+        if (res) {
+          payload = {
+            status: 200,
+            content: res,
+            correlationId: correlationId
+          }
+        }
+        //Send Response to acknowledge topic
+        let payloads = [
+          { topic: 'acknowledge', messages: JSON.stringify({ "acknowledgementpayload": true, payload }), partition: 0 }
+        ]
+        producer.send(payloads, (err, data) => {
+          if (err) throw err
+          console.log('ACK message sent:', data)
+        })
+      })
+    }
+
+    if (action === 'APPROVE_QUE'){
+      approve_question(payload, (err, res) => {
         var payload = {};
         if (err) {
           console.log('Get all Unapproved posts failed:', err)
