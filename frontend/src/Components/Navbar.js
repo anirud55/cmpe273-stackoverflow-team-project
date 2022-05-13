3310abbf094c920ea793153acfd4afa25282ee86
vineetkarmiani <vineetkarmiani@gmail.com>
2022-05-13 13:59:21 -0700

    Displaying badges in navbar

--- frontend/src/Components/Navbar.js
+++ frontend/src/Components/Navbar.js
@@ -8,7 +8,7 @@ import "./css/Navbar.css";
 import Searchbar from "./Searchbar";
 import { signout, isAutheticated } from "../auth/helper/authapicalls";
 import CircleIcon from "@mui/icons-material/Circle";
-
+import { API } from "../../src/backend";
 function NavBar({ history }) {
   const badges = { gold: 0, silver: 1, bronze: 0 }; // change after getting api call for badges
   // const history = useHistory();
@@ -18,6 +18,10 @@ function NavBar({ history }) {
   const [reload, setReload] = useState(false);
   const handleLoginClick = () => history.push("/login");
   const handleSignupClick = () => history.push("/signup");
+  const [gold, setGold] = useState(0);
+  const [silver, setSilver] = useState(0);
+  const [bronze, setBronze] = useState(0);
+
 
   function stringAvatar(name) {
     return {
@@ -50,8 +54,34 @@ function NavBar({ history }) {
     signout();
     document.location.reload();
   };
+  useEffect(() => {
+    const getBadges = ()=>{
+      return fetch(`${API}/badges/83`, {
+        method: "GET"
+      })
+        .then((response) => {
+          return response.json();
+        })
+        .then((res) => {
+          let gold = 0, silver=0, bronze=0;
+          res.map((r)=>{
+            if(r.badge_type==="gold")
+              gold++;
+            else if(r.badge_type==="silver")
+              silver++;
+            else if(r.badge_type==="bronze")
+              bronze++;
+          })
+          setGold(gold);
+          setBronze(bronze);
+          setSilver(silver);
+          console.log(res);
+        })
+        .catch((err) => console.log(err));
+    }
+    getBadges();
+  }, []);
 
-  useEffect(() => {}, []);
 
   return (
     <header>
@@ -96,13 +126,13 @@ function NavBar({ history }) {
                   alt={"profile"}
                   // {...stringAvatar(user)}
                 />
-                <p>45</p>
+                <p></p>
                 <CircleIcon sx={{ color: "gold", width: "10px" }} />
-                <p>1</p>
+                <p>{gold}</p>
                 <CircleIcon sx={{ color: "silver", width: "10px" }} />
-                <p>1</p>
+                <p>{silver}</p>
                 <CircleIcon sx={{ color: "#CD7F32", width: "10px" }} />
-                <p>1</p>
+                <p>{bronze}</p>
               </>
             )}
             &nbsp;&nbsp;
