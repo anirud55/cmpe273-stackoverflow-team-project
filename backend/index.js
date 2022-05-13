03fbf3c3396dbedbf8f3f775aee80ef3c703a0c1
Sudheendra Katikar <sudhu.katikar@gmail.com>
2022-05-13 13:04:05 -0700

    serve static build

--- backend/index.js
+++ backend/index.js
@@ -10,15 +10,13 @@ app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 app.use('/api', routes);
 
-app.get('/', (req, res) => {
-  app.use(express.static(path.join(__dirname, '../frontend/build')));
-});
+app.use(express.static('../frontend/build'))
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
