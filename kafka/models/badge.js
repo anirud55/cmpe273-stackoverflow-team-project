ff5b04d7b12bed056762ba50101c5dfe76016d62
Unmesh Padhye <unmeshpadhye@gmail.com>
2022-05-13 13:52:35 -0700

    cleanup with Badge model and kafka call

--- kafka/models/badge.js
+++ kafka/models/badge.js
@@ -1,33 +1,3 @@
-// import Sequelize from 'sequelize';
-
-// const Badge = sequelize.define('Badge', {
-//   id: {
-//     type: Sequelize.INTEGER,
-//     autoIncrement: true,
-//     primaryKey: true,
-//     allowNull: false
-//   },
-//   badge_name: {
-//     type: Sequelize.STRING,
-//     allowNull: false
-//   },
-//   badge_type: {
-//     type: Sequelize.STRING,
-//     allowNull: false
-//   },
-//   user_id: {
-//     type: Sequelize.STRING,
-//     allowNull: false
-//   },
-// },
-//   {
-//     timestamps: true,
-//     updatedAt: false
-//   }
-// )
-
-// export default Badge
-
 import sequelize from '../loaders/sql';
 import { DataTypes } from "sequelize";
 
