2fe22ce4dac029f05ccc4de893fce900ed4f5760
Sudheendra Katikar <sudhu.katikar@gmail.com>
2022-05-13 00:40:30 -0700

    bookmark routes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -14,7 +14,7 @@ router.get('/', async (req, res) => {
   });
 });
 
-router.get('/:id', async (req, res) => {
+router.get('/profile/:id', async (req, res) => {
   const { id } = req.params;
   sendRequest('users', { id, action: 'GET_USER_PROFILE' }, (err, data) => {
     if (err) {
@@ -24,5 +24,29 @@ router.get('/:id', async (req, res) => {
       res.status(200).json(data);
   });
 });
+
+router.post('/bookmark', async (req, res) => {
+  const { userId, postId } = req.body
+  sendRequest('users', { userId, postId, action: 'BOOKMARK_POST' }, (err, data) => {
+    if (err) {
+      res.status(400).json(err);
+    }
+    else
+      res.status(200).json(data);
+  })
+})
+
+router.get('/bookmark/:id', async (req, res) => {
+  console.log(req)
+  const { id } = req.params;
+  console.log(id)
+  sendRequest('users', { id, action: 'GET_BOOKMARKS' }, (err, data) => {
+    if (err) {
+      res.status(400).json(err);
+    }
+    else
+      res.status(200).json(data);
+  })
+})
 
 export default router;
